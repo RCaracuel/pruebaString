79865a4553d335b4fcc59c1727d18549c5bec39e
RCaracuel <rosa.caracuel@gmail.com>
2019-04-22 12:36:21 +0200

    Revert "C1"
    This reverts commit 127aa3e8259b8a73e48577690834c9119debb067.
    
    # Conflicts:
    #	src/clasestring/ClaseString.java

--- src/clasestring/ClaseString.java
+++ src/clasestring/ClaseString.java
@@ -13,10 +13,9 @@ public class ClaseString {
 
    
     public static void main(String[] args) {
-       
-        //cambio x
-        String s = "RosaCaracuel.com";
         
+        String s = "yo.com";
+        // Hola 
       
         System.out.println("Tamaños: " + s.length());
         
@@ -26,7 +25,7 @@ public class ClaseString {
         // Obtener subcadena desde el caracter i-ésimo hasta el final
         // de la cadena
         String subcadena = s.substring(3);
-        System.out.println("Substring = " + subcadena);
+        System.out.println("Subcadena = " + subcadena);
        
         // Obtener subcadena desde el caracter i-ésimo hasta el 
         // caracter j-ésimo de la cadena
